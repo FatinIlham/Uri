2a48b0a1a8be3833e16167f5b3f6deee37a5be1e
Fatin Ilham <61835955+FatinIlham@users.noreply.github.com>
2021-03-25 15:06:27 +0600

    Add files via upload

---
+++ Uri 1009.c
@@ -0,0 +1,19 @@
+#include<stdio.h>
+int main()
+{
+  float a,b,TOTAL;
+  printf("JOAO\n");
+  scanf("%f",&a);
+  scanf("%f",&b);
+
+  TOTAL = a+((b*15)/100);
+  printf("TOTAL = %f\n",TOTAL);
+
+
+
+
+
+
+
+
+}
